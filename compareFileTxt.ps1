a71f5988a51186b35ab3e69aa8d58dce2a4a869d
Maya Islambouli <43270477+MayaIsla@users.noreply.github.com>
2022-06-08 15:17:35 -0500

    Update compareFileTxt.ps1

--- compareFileTxt.ps1
+++ compareFileTxt.ps1
@@ -1,5 +1,5 @@
-$files1 = "C:\Users\mislambo\OneDrive - Western Alliance Bank\Desktop\t.properties"
-$files2 = "C:\Users\mislambo\OneDrive - Western Alliance Bank\Desktop\s.properties"
+$files1 = "C:\Users\filepath\Desktop\t.properties"
+$files2 = "C:\Users\filepath\Desktop\s.properties"
 
 
 compare-object (get-content $files2) (get-content $files1) | select -ExpandProperty InputObject| out-file $files2  -append -Encoding utf8
