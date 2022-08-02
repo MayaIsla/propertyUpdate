0df0fe60b02ab903945968f9c931e8b473a01ff2
Maya Islambouli <43270477+MayaIsla@users.noreply.github.com>
2022-08-02 10:25:24 -0500

    Update compareFileTxt.ps1
    Creates a backup of the original property file. In this case, it's (sb.properties). s.properties is your original file, u.properties is your NEW file. This will cause properties to be overwritten. This is normal as u.properties will be your new file.

--- compareFileTxt.ps1
+++ compareFileTxt.ps1
@@ -1,5 +1,14 @@
-$files1 = "C:\Users\filepath\Desktop\t.properties"
-$files2 = "C:\Users\filepath\Desktop\s.properties"
+#init var property files on desktop
+
+$files1 = "C:\Users\mislambo\OneDrive - Western Alliance Bank\Desktop\t.properties" #requested property file
+$files2 = "C:\Users\mislambo\OneDrive - Western Alliance Bank\Desktop\s.properties" #original file
+$files3 = "C:\Users\mislambo\OneDrive - Western Alliance Bank\Desktop\u.properties" #Your new property file
+
+Add-Content -Path "C:\Users\mislambo\OneDrive - Western Alliance Bank\Desktop\sb.properties" -Value (Get-Content -Path $files2)
+Get-Content -Path $files2 #create a backup of s.properties (your main property original file)
+
+compare-object (get-content $files1) (get-content $files2) | select -ExpandProperty InputObject | out-file $files2  -append -Encoding utf8 #compares and pastes vars that do not exist into new original file
 
 
-compare-object (get-content $files2) (get-content $files1) | select -ExpandProperty InputObject| out-file $files2  -append -Encoding utf8
+Get-Content $files2 | Group-Object {$_.Split(' = ')[0] } | ForEach-Object {$_.Group[-1] } | Out-File $files3 -encoding utf8
+
